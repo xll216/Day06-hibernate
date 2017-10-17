522dd8856040e1fa3cf7c52017ac798e1bc938cc
xll216 <xll_216@163.com>
2017-10-17 18:34:00 +0800

    添加find和findAll的单元测试方法 作者：肖玲玲 时间：2017.10.17

--- src/com/lanou/test/StudentDaoTest.java
+++ src/com/lanou/test/StudentDaoTest.java
@@ -5,6 +5,10 @@ import com.lanou.dao.impl.StudentDaoImpl;
 import com.lanou.domain.Student;
 import org.junit.Test;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * Created by 蓝鸥科技有限公司  www.lanou3g.com.
  */
@@ -25,6 +29,30 @@ public class StudentDaoTest {
 
         System.out.println("保存后：" + student);
 
+    }
+
+    @Test
+    public void find() {
+        StudentDao dao = new StudentDaoImpl();
+        String hql = "from Student where sname=:name and age=:age";
+        Map<String, Object> params = new HashMap<>();
+        params.put("name", "马祥光");//对应冒号后面的值
+        params.put("age", 24);
+        Student student = dao.find(hql, params);
+        System.out.println(student);
+
+    }
+
+    @Test
+    public void findAll() {
+        StudentDao dao = new StudentDaoImpl();
+        String hql = "from Student";
+
+        List<Student> students = dao.findAll(hql, null);
+
+        for (Student stu : students) {
+            System.out.println(stu);
+        }
     }
 
     @Test
